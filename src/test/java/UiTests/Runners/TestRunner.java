424c28055a14fee21c9fec98dc074f9e07e54051
martov656 <martintovarek2@gmail.com>
2025-06-06 08:29:06 +0200

    Test

--- src/test/java/UiTests/Runners/TestRunner.java
+++ src/test/java/UiTests/Runners/TestRunner.java
@@ -13,5 +13,5 @@ import org.junit.platform.suite.api.Suite;
 @ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME,value = "@regression")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
-public class TestRunner {
+public class TestRunner  {
 }
